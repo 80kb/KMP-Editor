9675774596ee2a3d1b600ad026e8d846ce299abd
J Henning <jonathanchenning@outlook.com>
2024-04-03 19:55:02 -0400

    Basic UI design and function

--- KMP Editor/MainForm.Designer.cs
+++ KMP Editor/MainForm.Designer.cs
@@ -28,45 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            TreeNode treeNode1 = new TreeNode("Start Position(s)");
+            TreeNode treeNode2 = new TreeNode("Enemy Routes");
+            TreeNode treeNode3 = new TreeNode("Item Routes");
+            TreeNode treeNode4 = new TreeNode("Checkpoints");
+            TreeNode treeNode5 = new TreeNode("Objects");
+            TreeNode treeNode6 = new TreeNode("Routes");
+            TreeNode treeNode7 = new TreeNode("Areas");
+            TreeNode treeNode8 = new TreeNode("Cameras");
+            TreeNode treeNode9 = new TreeNode("Respawns");
+            TreeNode treeNode10 = new TreeNode("Cannons");
+            TreeNode treeNode11 = new TreeNode(" Battle Endpoints");
+            TreeNode treeNode12 = new TreeNode("Stage Info");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            menuStrip1 = new MenuStrip();
+            fileMenuItem = new ToolStripMenuItem();
+            newMenuItem = new ToolStripMenuItem();
+            openMenuItem = new ToolStripMenuItem();
+            saveMenuItem = new ToolStripMenuItem();
+            saveAsMenuItem = new ToolStripMenuItem();
+            sectionTree = new TreeView();
+            entryListBox = new ListBox();
+            entryPropertyGrid = new PropertyGrid();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // menuStrip1
             // 
-            button1.Location = new Point(337, 138);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Open";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
             // 
-            // button2
+            // fileMenuItem
             // 
-            button2.Location = new Point(337, 266);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "Save";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            fileMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newMenuItem, openMenuItem, saveMenuItem, saveAsMenuItem });
+            fileMenuItem.Name = "fileMenuItem";
+            fileMenuItem.Size = new Size(37, 20);
+            fileMenuItem.Text = "File";
+            // 
+            // newMenuItem
+            // 
+            newMenuItem.Image = Properties.Resources.page_white_add;
+            newMenuItem.Name = "newMenuItem";
+            newMenuItem.Size = new Size(121, 22);
+            newMenuItem.Text = "New";
+            newMenuItem.Click += newMenuItem_Click;
+            // 
+            // openMenuItem
+            // 
+            openMenuItem.Image = Properties.Resources.folder;
+            openMenuItem.Name = "openMenuItem";
+            openMenuItem.Size = new Size(121, 22);
+            openMenuItem.Text = "Open...";
+            openMenuItem.Click += openMenuItem_Click;
+            // 
+            // saveMenuItem
+            // 
+            saveMenuItem.Image = Properties.Resources.disk;
+            saveMenuItem.Name = "saveMenuItem";
+            saveMenuItem.Size = new Size(121, 22);
+            saveMenuItem.Text = "Save";
+            saveMenuItem.Click += saveMenuItem_Click;
+            // 
+            // saveAsMenuItem
+            // 
+            saveAsMenuItem.Name = "saveAsMenuItem";
+            saveAsMenuItem.Size = new Size(121, 22);
+            saveAsMenuItem.Text = "Save as...";
+            saveAsMenuItem.Click += saveAsMenuItem_Click;
+            // 
+            // sectionTree
+            // 
+            sectionTree.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            sectionTree.Enabled = false;
+            sectionTree.Location = new Point(12, 27);
+            sectionTree.Name = "sectionTree";
+            treeNode1.Name = "ktptNode";
+            treeNode1.Text = "Start Position(s)";
+            treeNode2.Name = "enptNode";
+            treeNode2.Text = "Enemy Routes";
+            treeNode3.Name = "itptNode";
+            treeNode3.Text = "Item Routes";
+            treeNode4.Name = "ckptNode";
+            treeNode4.Text = "Checkpoints";
+            treeNode5.Name = "gobjNode";
+            treeNode5.Text = "Objects";
+            treeNode6.Name = "potiNode";
+            treeNode6.Text = "Routes";
+            treeNode7.Name = "areaNode";
+            treeNode7.Text = "Areas";
+            treeNode8.Name = "cameNode";
+            treeNode8.Text = "Cameras";
+            treeNode9.Name = "jgpt";
+            treeNode9.Text = "Respawns";
+            treeNode10.Name = "cnptNode";
+            treeNode10.Text = "Cannons";
+            treeNode11.Name = "msptNode";
+            treeNode11.Text = " Battle Endpoints";
+            treeNode12.Name = "stgiNode";
+            treeNode12.Text = "Stage Info";
+            sectionTree.Nodes.AddRange(new TreeNode[] { treeNode1, treeNode2, treeNode3, treeNode4, treeNode5, treeNode6, treeNode7, treeNode8, treeNode9, treeNode10, treeNode11, treeNode12 });
+            sectionTree.Size = new Size(174, 411);
+            sectionTree.TabIndex = 1;
+            // 
+            // entryListBox
+            // 
+            entryListBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            entryListBox.FormattingEnabled = true;
+            entryListBox.ItemHeight = 15;
+            entryListBox.Location = new Point(594, 27);
+            entryListBox.Name = "entryListBox";
+            entryListBox.Size = new Size(194, 169);
+            entryListBox.TabIndex = 2;
+            // 
+            // entryPropertyGrid
+            // 
+            entryPropertyGrid.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            entryPropertyGrid.Location = new Point(594, 202);
+            entryPropertyGrid.Name = "entryPropertyGrid";
+            entryPropertyGrid.Size = new Size(194, 236);
+            entryPropertyGrid.TabIndex = 3;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(entryPropertyGrid);
+            Controls.Add(entryListBox);
+            Controls.Add(sectionTree);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
             Name = "MainForm";
             Text = "KMP Editor";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileMenuItem;
+        private ToolStripMenuItem newMenuItem;
+        private ToolStripMenuItem openMenuItem;
+        private ToolStripMenuItem saveMenuItem;
+        private ToolStripMenuItem saveAsMenuItem;
+        private TreeView sectionTree;
+        private ListBox entryListBox;
+        private PropertyGrid entryPropertyGrid;
     }
 }
--- KMP Editor/MainForm.cs
+++ KMP Editor/MainForm.cs
@@ -4,14 +4,23 @@ namespace KMP_Editor
 {
     public partial class MainForm : Form
     {
-        KMP CurrentFile;
+        private KMP FileInstance;
 
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Populate()
+        {
+            if (FileInstance == null)
+                return;
+
+            sectionTree.Enabled = true;
+
+        }
+
+        private void openMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "KMP Files (*.kmp)|*.kmp|All Files (*.*)|*.*";
@@ -19,19 +28,43 @@ namespace KMP_Editor
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 byte[] buffer = File.ReadAllBytes(ofd.FileName);
-                CurrentFile = new KMP(buffer, ofd.FileName);
+                FileInstance = new KMP(buffer, ofd.FileName);
+                Text = "KMP Editor - " + Path.GetFileName(FileInstance.Filename);
+                Populate();
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void saveMenuItem_Click(object sender, EventArgs e)
         {
+            if (FileInstance != null && File.Exists(FileInstance.Filename))
+            {
+                File.WriteAllBytes(FileInstance.Filename, FileInstance.Write());
+            }
+            else if (FileInstance != null)
+            {
+                saveAsMenuItem_Click(sender, e);
+            }
+        }
+
+        private void saveAsMenuItem_Click(object sender, EventArgs e)
+        {
+            if (FileInstance == null)
+                return;
+
             SaveFileDialog sfd = new SaveFileDialog();
             sfd.Filter = "KMP Files (*.kmp)|*.kmp|All Files (*.*)|*.*";
 
             if (sfd.ShowDialog() == DialogResult.OK)
             {
-                File.WriteAllBytes(sfd.FileName, CurrentFile.Write());
+                File.WriteAllBytes(sfd.FileName, FileInstance.Write());
             }
+        }
+
+        private void newMenuItem_Click(object sender, EventArgs e)
+        {
+            FileInstance = new KMP();
+            Text = "KMP Editor - " + Path.GetFileName(FileInstance.Filename);
+            Populate();
         }
     }
 }
